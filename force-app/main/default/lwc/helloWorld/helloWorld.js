002bfe0b76cc1872011c568782c0e8b9ecc16733
ramakrishna.nakkina <ramakrishna.nakkina@gmail.com>
2022-04-14 00:00:11 -0400

    two-way

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -2,5 +2,8 @@ import { LightningElement } from 'lwc';
 
 export default class HelloWorld extends LightningElement {
 fullname = "ramakrishna"
-
+changeHandler(event)
+{
+   this.title = event.target.value
+}
 }
