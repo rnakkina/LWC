b5802cc896986eb6a852f0906c35d1ae6446819b
ramakrishna.nakkina <ramakrishna.nakkina@gmail.com>
2022-04-16 11:32:16 -0400

    hh

--- force-app/main/default/lwc/loopingLwc/loopingLwc.js
+++ force-app/main/default/lwc/loopingLwc/loopingLwc.js
@@ -3,5 +3,26 @@ import { LightningElement } from 'lwc';
 export default class LoopingLwc extends LightningElement {
 
     CarLst = ["nissan", "ford", "kia", "audi"]
-
+    ceoLst = [
+        {
+            id:1,
+            company:"Google",
+            name:"rama"
+        },
+        {
+            id:2,
+            company:"Apple",
+            name:"krishna"
+        },
+        {
+            id:3,
+            company:"amezon",
+            name:"nakkina"
+        },
+        {
+            id:4,
+            company:"facebook",
+            name:"raja"
+        }
+    ]
 }
