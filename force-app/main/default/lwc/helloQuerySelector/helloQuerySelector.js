407d4c7d88fd64d63ecc6e2bc158c86dc4e3d6b1
ramakrishna.nakkina <ramakrishna.nakkina@gmail.com>
2022-04-17 11:49:14 -0400

    inline

--- force-app/main/default/lwc/helloQuerySelector/helloQuerySelector.js
+++ force-app/main/default/lwc/helloQuerySelector/helloQuerySelector.js
@@ -1,9 +1,18 @@
 import { LightningElement } from 'lwc';
-
 export default class HelloQuerySelector extends LightningElement {
+    lst = ['rama' , 'krishna' , 'nakkina', 'sneha']
     fetchHandler()
     {
         const elem=this.template.querySelector('h1')
         console.log(elem.innerText)
+        elem.style.border = "1px solid red";
+
+        const lstelem = this.template.querySelectorAll('.name')
+        Array.from(lstelem).forEach(item=>{
+            console.log(item.innerText)
+            item.setAttribute("title", item.innerText)
+        })
+        const childelem = this.template.querySelector('.child')
+        childelem.innerHTML='<p> hey this is child </p>'
     }
 }
