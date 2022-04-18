ada907d5b942cb90ca9a35010e7b2c91843b02b0
ramakrishna.nakkina <ramakrishna.nakkina@gmail.com>
2022-04-18 17:39:51 -0400

    u

---
+++ force-app/main/default/lwc/p2cParentComponent/p2cParentComponent.js
@@ -0,0 +1,21 @@
+import { LightningElement } from 'lwc';
+
+export default class P2cParentComponent extends LightningElement {
+    carouselData=[
+        {
+                    src : "https://www.lightningdesignsystem.com/assets/images/carousel/carousel-01.jpg",
+                    header : "First Card",
+                    description : "First card description."
+        },
+        {
+            src : "https://www.lightningdesignsystem.com/assets/images/carousel/carousel-02.jpg",
+            header : "second Card",
+            description : "second card description."
+        },
+        {
+            src : "https://www.lightningdesignsystem.com/assets/images/carousel/carousel-03.jpg",
+            header : "third Card",
+            description : "third card description."
+        }
+    ]
+}
