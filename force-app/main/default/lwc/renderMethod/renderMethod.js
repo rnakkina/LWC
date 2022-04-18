f1200c8d6306a086ca6b81834c4ba266d2f041d7
ramakrishna.nakkina <ramakrishna.nakkina@gmail.com>
2022-04-17 21:05:48 -0400

    l

--- force-app/main/default/lwc/renderMethod/renderMethod.js
+++ force-app/main/default/lwc/renderMethod/renderMethod.js
@@ -1,3 +1,17 @@
 import { LightningElement } from 'lwc';
-
-export default class RenderMethod extends LightningElement {}
+import signinTemplate from './signinTemplate.html'
+import signupTemplate from './signupTemplate.html'
+import renderTemplate from './renderMethod.html'
+export default class RenderMethod extends LightningElement {
+    selectedbtn=''
+    render()
+    {
+        return this.selectedbtn === 'signup' ? signupTemplate :
+        this.selectedbtn === 'signin' ? signinTemplate :
+        renderTemplate
+    }
+    handleClick(event)
+    {
+        this.selectedbtn = event.target.lable
+    }
+}
