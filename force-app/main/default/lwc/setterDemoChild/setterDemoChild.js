a3fb3a9d92536cf804ccbffbb90bf246adc8184e
ramakrishna.nakkina <ramakrishna.nakkina@gmail.com>
2022-04-18 22:45:18 -0400

    d

---
+++ force-app/main/default/lwc/setterDemoChild/setterDemoChild.js
@@ -0,0 +1,5 @@
+import { LightningElement, api } from 'lwc';
+
+export default class SetterDemoChild extends LightningElement {
+    @api detail
+}
---
+++ force-app/main/default/lwc/setterDemoParent/setterDemoParent.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+
+export default class SetterDemoParent extends LightningElement {
+    userdetails={
+        name:"rama",
+        age:35
+    }
+}
