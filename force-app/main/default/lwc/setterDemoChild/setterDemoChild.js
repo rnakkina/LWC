71c25bbed515aa4ab6bda1161f382a620b3cd8dd
ramakrishna.nakkina <ramakrishna.nakkina@gmail.com>
2022-04-19 12:45:09 -0400

    lk

--- force-app/main/default/lwc/setterDemoChild/setterDemoChild.js
+++ force-app/main/default/lwc/setterDemoChild/setterDemoChild.js
@@ -1,5 +1,15 @@
 import { LightningElement, api } from 'lwc';
 
 export default class SetterDemoChild extends LightningElement {
-    @api detail
+    userDetail
+
+    @api
+    get detail()
+    {
+        return this.userDetail
+    }
+    set detail(data){
+        let newage = data.age*2
+        this.userDetail = {...data, age:newage, "location":"vijayawada"}
+    }
 }
