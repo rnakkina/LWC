1462473b5f980bac5ddf2f79fb4d30e725b8ff90
ramakrishna.nakkina <ramakrishna.nakkina@gmail.com>
2022-04-19 22:52:15 -0400

    aura comm

--- force-app/main/default/lwc/customLightningCard/customLightningCard.js
+++ force-app/main/default/lwc/customLightningCard/customLightningCard.js
@@ -1,12 +1,13 @@
 import { LightningElement } from 'lwc';
 
-export default class SlotChildDemo extends LightningElement {
-    
-    handlefooterchange(){
+export default class CustomLightningCard extends LightningElement {
+    handlefooterchange()
+    {
         const footerElem = this.template.querySelector('.slds-card__footer');
         if(footerElem)
         {
             footerElem.classList.remove('slds-hide')
         }
     }
+
 }
