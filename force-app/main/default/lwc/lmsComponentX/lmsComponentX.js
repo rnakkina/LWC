1ba3d7337e46068a9abb398bd9f80f1b167a03b4
ramakrishna.nakkina <ramakrishna.nakkina@gmail.com>
2022-04-20 21:51:53 -0400

    h

--- force-app/main/default/lwc/lmsComponentX/lmsComponentX.js
+++ force-app/main/default/lwc/lmsComponentX/lmsComponentX.js
@@ -1,3 +1,21 @@
-import { LightningElement } from 'lwc';
+import { LightningElement , wire} from 'lwc';
+import SAMPLEMC from "@salesforce/messageChannel/SampleMessageChannel__c"
+import {APPLICATION_SCOPE, MessageContext, subscribe} from 'lightning/messageService'
+export default class LmsComponentX extends LightningElement {
+    recievedMessage
+    @wire(MessageContext)
+    context
+    connectedCallback()
+    {
+        this.subscribeMessage()
+    }
+    subscribeMessage(){
+        subscribe(this.Context, SAMPLEMC, (message)=>{
+            this.handleMessage(message)}, {scope:APPLICATION_SCOPE})
+    }
+    handleMessage(message)
+    {
+        this.recievedMessage =  message.lmsData.value ? message.lmsData.value : "no message published"
+    }
 
-export default class LmsComponentX extends LightningElement {}
+}
