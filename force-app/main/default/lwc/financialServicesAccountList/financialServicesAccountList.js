0df7537ff836e440fed49f56c8b00c03d6842182
ramakrishna.nakkina <ramakrishna.nakkina@gmail.com>
2022-05-16 22:31:58 -0400

    ff

--- force-app/main/default/lwc/financialServicesAccountList/financialServicesAccountList.js
+++ force-app/main/default/lwc/financialServicesAccountList/financialServicesAccountList.js
@@ -3,26 +3,33 @@ import searchAccount from '@salesforce/apex/FsAccounts.searchAccount'
 import allFsAccounts from '@salesforce/apex/FsAccounts.allFsAccounts'
 import { updateRecord  } from 'lightning/uiRecordApi';
 const COLS=[
-    {label:'Account Name', fieldName:'Name', editable:true},
-    {label:'Phone', fieldName:'Phone', editable:true},
-    {label:'Website', fieldName:'Website', editable:true},
-    {label:'Annual Revenue', fieldName:'AnnualRevenue', editable:true},
-    {label:'Account Owner', fieldName:'Owner.Name'}
+    {label:'Account Name', fieldName:'Name', editable:true, sortable: "true"},
+    {label:'Phone', fieldName:'Phone',  type:"phone",   sortable: "false" , editable:true},
+    {label:'Website', fieldName:'Website',  type:"url",    sortable: "false", editable:true},
+    {label:'Annual Revenue', fieldName:'AnnualRevenue', sortable: "false", editable:true},
+    {label:'Account Owner', fieldName:'OwnerName', sortable: "true"}
 ]
 export default class FinancialServicesAccountList extends LightningElement {
-    searchkey=''
     @track accountsList = []
-    filteredAccounts = []
+    @track filteredAccounts = []
     columns = COLS
     draftValues=[]
+    @track sortBy
+    @track sortDirection
     @wire(allFsAccounts)
     fsAccountsData({data, error})
     {
         if(data)
         {
             console.log(data)
-            this.accountsList = data
-            this.filteredAccounts = data
+            let tempAccList = [];
+            data.forEach((record)=>{
+                let tempAccRec = Object.assign({}, record);
+                tempAccRec.OwnerName =  record.Owner.Name;
+                tempAccList.push(tempAccRec);
+            })
+            this.accountsList = tempAccList
+            this.filteredAccounts = tempAccList
         }
         if(error)
         {
@@ -33,11 +40,9 @@ export default class FinancialServicesAccountList extends LightningElement {
     onkeyupHandler(event)
     {
         console.log(event.target.value)
-        this.searchkey = event.target.value
         let src = event.target.value
         this.filteredAccounts = []
         let accf = this.accountsList.map(item=> {
-        console.log(item.Name.includes(src))
         if(item.Name.includes(src)){
             this.filteredAccounts.push(item)
         }
@@ -58,4 +63,22 @@ export default class FinancialServicesAccountList extends LightningElement {
         })
         
     }
+    doSorting(event) {
+        this.sortBy = event.detail.fieldName;
+        this.sortDirection = event.detail.sortDirection;
+        this.sortData(this.sortBy, this.sortDirection);
+    }
+    sortData(fieldname, direction) {
+        let parseData = JSON.parse(JSON.stringify(this.filteredAccounts));
+        let keyValue = (a) => {
+            return a[fieldname];
+        };
+        let isReverse = direction === 'asc' ? 1: -1;
+        parseData.sort((x, y) => {
+            x = keyValue(x) ? keyValue(x) : ''; 
+            y = keyValue(y) ? keyValue(y) : '';
+            return isReverse * ((x > y) - (y > x));
+        });
+        this.filteredAccounts = parseData;
+    }    
 }
